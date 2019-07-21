efb1c2e063b9fa03f9920e2c827bf2fc837fd256
wtrbrd <nick.valdez@wpengine.com>
2019-07-21 15:49:19 -0500

    updating views and shit

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views 
 
+app_name='polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
@@ -12,3 +13,4 @@ urlpatterns = [
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
 
 from .models import Question
 
 def index(request):
-        latest_question_list = Question.objects.order_by('-pub_date')[:5]
-        template = loader.get_template('polls/index.html')
-        context = {
-        'latest_question_list': latest_question_list,
-        }
-        return HttpResponse(template.render(context, request))
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  context = {'latest_question_list': latest_question_list}
+  return render(request, 'polls/index.html', context)
+       
 # Create your views here.
 
-def detail(request, question_id): 
-    return HttpResponse("You are looking at question %s. " % question_id)
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("It's not here dude!")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of the question %s."
@@ -22,3 +25,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+
